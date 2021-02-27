6e5a7af757024a5a02923aada29366d305198803
Andrew Dimmer <contactandrewd@gmail.com>
2021-02-27 15:14:01 -0500

    feat(server): Added API Explorer
    Because the server is not actually used to host the webapp, just the API, where the webapp used to be in the server file structure is now a development tool to test API endpoints.

--- server/src/public/scripts/index.js
+++ server/src/public/scripts/index.js
@@ -1,171 +1,86 @@
 /******************************************************************************
- *                          Fetch and display users
+ *                                  Send Requests
  ******************************************************************************/
 
-displayUsers();
-
-
-function displayUsers() {
-    httpGet('/api/users/all')
-        .then(response => response.json())
-        .then((response) => {
-            var allUsers = response.users;
-            // Empty the anchor
-            var allUsersAnchor = document.getElementById('all-users-anchor');
-            allUsersAnchor.innerHTML = '';
-            // Append users to anchor
-            allUsers.forEach((user) => {
-                allUsersAnchor.innerHTML += getUserDisplayEle(user);
-            });
-        });
-};
-
-
-function getUserDisplayEle(user) {
-    return `<div class="user-display-ele">
-
-        <div class="normal-view">
-            <div>Name: ${user.name}</div>
-            <div>Email: ${user.email}</div>
-            <button class="edit-user-btn" data-user-id="${user.id}">
-                Edit
-            </button>
-            <button class="delete-user-btn" data-user-id="${user.id}">
-                Delete
-            </button>
-        </div>
-        
-        <div class="edit-view">
-            <div>
-                Name: <input class="name-edit-input" value="${user.name}">
-            </div>
-            <div>
-                Email: <input class="email-edit-input" value="${user.email}">
-            </div>
-            <button class="submit-edit-btn" data-user-id="${user.id}">
-                Submit
-            </button>
-            <button class="cancel-edit-btn" data-user-id="${user.id}">
-                Cancel
-            </button>
-        </div>
-    </div>`;
-}
-
-
-/******************************************************************************
- *                        Add, Edit, and Delete Users
- ******************************************************************************/
-
-document.addEventListener('click', function (event) {
+document.addEventListener(
+  "click",
+  function (event) {
     event.preventDefault();
     var ele = event.target;
-    if (ele.matches('#add-user-btn')) {
-        addUser();
-    } else if (ele.matches('.edit-user-btn')) {
-        showEditView(ele.parentNode.parentNode);
-    } else if (ele.matches('.cancel-edit-btn')) {
-        cancelEdit(ele.parentNode.parentNode);
-    } else if (ele.matches('.submit-edit-btn')) {
-        submitEdit(ele);
-    } else if (ele.matches('.delete-user-btn')) {
-        deleteUser(ele);
+    var path = document.getElementById("endpoint-input").value;
+    var data = document.getElementById("body-input").value;
+    if (ele.matches("#get-btn")) {
+      sendRequest(path, data, httpGet);
+    } else if (ele.matches(".post-btn")) {
+      sendRequest(path, data, httpPost);
+    } else if (ele.matches(".patch-btn")) {
+      sendRequest(path, data, httpPatch);
+    } else if (ele.matches(".delete-btn")) {
+      sendRequest(path, data, httpDelete);
     }
-}, false)
+  },
+  false
+);
 
+function sendRequest(path, data, httpClientFunction) {
+  // Clear old response
+  displayResponses({ status: "", bodyOverride: "" });
 
-function addUser() {
-    var nameInput = document.getElementById('name-input');
-    var emailInput = document.getElementById('email-input');
-    var data = {
-        user: {
-            name: nameInput.value,
-            email: emailInput.value
-        },
-    };
-    httpPost('/api/users/add', data)
-        .then(() => {
-            displayUsers();
-        })
+  // Send request
+  httpGet(path, data).then((response) => {
+    // Display new response
+    displayResponses(response);
+  });
 }
-
-
-function showEditView(userEle) {
-    var normalView = userEle.getElementsByClassName('normal-view')[0];
-    var editView = userEle.getElementsByClassName('edit-view')[0];
-    normalView.style.display = 'none';
-    editView.style.display = 'block';
-}
-
-
-function cancelEdit(userEle) {
-    var normalView = userEle.getElementsByClassName('normal-view')[0];
-    var editView = userEle.getElementsByClassName('edit-view')[0];
-    normalView.style.display = 'block';
-    editView.style.display = 'none';
-}
-
-
-function submitEdit(ele) {
-    var userEle = ele.parentNode.parentNode;
-    var nameInput = userEle.getElementsByClassName('name-edit-input')[0];
-    var emailInput = userEle.getElementsByClassName('email-edit-input')[0];
-    var id = ele.getAttribute('data-user-id');
-    var data = {
-        user: {
-            name: nameInput.value,
-            email: emailInput.value,
-            id: id
-        }
-    };
-	httpPut('/api/users/update', data)
-        .then(() => {
-            displayUsers();
-        })
-}
-
-
-function deleteUser(ele) {
-    var id = ele.getAttribute('data-user-id');
-	httpDelete('/api/users/delete/' + id)
-        .then(() => {
-            displayUsers();
-        })
-}
-
 
 function httpGet(path) {
-    return fetch(path, getOptions('GET'))
+  return fetch(path, getOptions("GET"));
 }
-
 
 function httpPost(path, data) {
-    return fetch(path, getOptions('POST', data));
+  return fetch(path, getOptions("POST", data));
 }
-
 
 function httpPut(path, data) {
-    return fetch(path, getOptions('PUT', data));
+  return fetch(path, getOptions("PUT", data));
 }
 
+function httpPatch(path, data) {
+  return fetch(path, getOptions("PATCH", data));
+}
 
 function httpDelete(path) {
-    return fetch(path, getOptions('DELETE'));
+  return fetch(path, getOptions("DELETE"));
 }
-
 
 function getOptions(verb, data) {
-    var options = {
-        dataType: 'json',
-        method: verb,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    };
-    if (data) {
-        options.body = JSON.stringify(data);
-    }
-    return options;
+  var options = {
+    dataType: "json",
+    method: verb,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  if (data) {
+    options.body = JSON.stringify(data);
+  }
+  return options;
 }
 
+/******************************************************************************
+ *                               Display Responses
+ ******************************************************************************/
+
+function displayResponses(response) {
+  document.getElementById("status-output").value = response.status;
+  if (response.bodyOverride === undefined) {
+    response.text().then((body) => {
+      document.getElementById("body-output").value = body;
+    });
+  } else {
+    document.getElementById(
+      "body-output"
+    ).value = response.bodyOverride.toString();
+  }
+}
